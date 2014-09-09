2498cceecd28932780e4dec244d84cd079d82d83
Doug Hellmann <doug@doughellmann.com>
2014-09-09 14:36:42 -0400

    Rename package to yasfb and use pbr for packaging

--- setup.py
+++ setup.py
@@ -1,34 +1,8 @@
-from distutils.core import setup
+#!/usr/bin/env python
 
-long_desc = open('README.rst').read()
-
-requires = [
-    'Sphinx>=0.6',
-    'feedformatter',
-    ]
+from setuptools import setup
 
 setup(
-    name='sphinxfeed',
-    version='0.3',
-    license='BSD',
-    author='junkafarian',
-    author_email='junkafarian@gmail.com',
-    url='https://github.com/junkafarian/sphinxfeed',
-    description='Sphinx extension for generating RSS feeds',
-    long_description=long_desc,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Documentation',
-        'Topic :: Utilities',
-    ],
-    platforms='any',
-    py_modules=['sphinxfeed'],
-    include_package_data=True,
-    install_requires=requires,
+    setup_requires=['pbr'],
+    pbr=True,
 )
