930563e5dd5b729354c743ab73e1911a319a18c4
Daniel Roth <danroth27@hotmail.com>
2015-11-19 15:49:45 -0800

    Update __init__.py

--- yasfb/__init__.py
+++ yasfb/__init__.py
@@ -109,7 +109,7 @@ def create_feed_item(app, pagename, templatename, ctx, doctree):
         'link': (app.config.feed_base_url
                  + '/'
                  + ctx['current_page_name']
-                 + app.builder.out_suffix,
+                 + '.html',
         'description': ctx.get('body'),
         'pubDate': pubdate,
     }
